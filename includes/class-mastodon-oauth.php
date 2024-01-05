f2c3c684287ce59ab29c46fcc7710a203b326467
Alex Kirk <akirk@users.noreply.github.com>
2024-01-05 16:14:31 +0100

    Fix interoperability with WPS Hide Login

--- includes/class-mastodon-oauth.php
+++ includes/class-mastodon-oauth.php
@@ -65,6 +65,7 @@ class Mastodon_OAuth {
 	}
 
 	public function handle_oauth( $return_value = false ) {
+		global $wp_query;
 		if ( 'POST' === $_SERVER['REQUEST_METHOD'] && empty( $_POST ) && ! empty( $_REQUEST ) ) {
 			$_POST = $_REQUEST;
 		}
@@ -74,6 +75,7 @@ class Mastodon_OAuth {
 				if ( get_option( 'mastodon_api_disable_logins' ) ) {
 					return null;
 				}
+				$wp_query->is_404 = false;
 				$handler = new OAuth2\AuthorizeHandler( $this->server );
 				break;
 
@@ -86,10 +88,12 @@ class Mastodon_OAuth {
 					exit;
 				}
 				header( 'Access-Control-Allow-Origin: *' );
+				$wp_query->is_404 = false;
 				$handler = new OAuth2\TokenHandler( $this->server );
 				break;
 
 			case '/oauth/revoke':
+				$wp_query->is_404 = false;
 				$handler = new OAuth2\RevokationHandler( $this->server );
 				break;
 		}
