f3c85562ec8da04b8808009afb8cf1274ee25c8a
Alex Kirk <akirk@users.noreply.github.com>
2024-01-14 19:31:15 +0100

    Add resolve support to the search

--- includes/class-mastodon-api.php
+++ includes/class-mastodon-api.php
@@ -1536,58 +1536,70 @@ class Mastodon_API {
 		$ret = array(
 			'accounts' => array(),
 			'statuses' => array(),
+			'hashtags' => array(),
 		);
-		if ( ! $type || 'accounts' === $type ) {
-			if ( preg_match( '/^@?' . self::ACTIVITYPUB_USERNAME_REGEXP . '$/i', $request->get_param( 'q' ) ) && ! $request->get_param( 'offset' ) ) {
-				$ret['accounts'][] = $this->get_friend_account_data( $request->get_param( 'q' ), array(), true );
-			}
-			$query = new \WP_User_Query(
-				array(
-					'search'         => '*' . $request->get_param( 'q' ) . '*',
-					'search_columns' => array(
-						'user_login',
-						'user_nicename',
-						'user_email',
-						'user_url',
-						'display_name',
-					),
-					'offset'         => $request->get_param( 'offset' ),
-					'number'         => $request->get_param( 'limit' ),
-				)
-			);
-			$users = $query->get_results();
-			foreach ( $users as $user ) {
-				$ret['accounts'][] = $this->get_friend_account_data( $user->ID );
-			}
-		}
-		if ( ! $type || 'statuses' === $type ) {
-			$args = $this->get_posts_query_args( $request );
-			if ( empty( $args ) ) {
-				return array();
-			}
-			$args = apply_filters( 'mastodon_api_timelines_args', $args, $request );
-			$valid_url = wp_parse_url( $request->get_param( 'q' ) );
-			if ( $valid_url && isset( $valid_url['host'] ) ) {
-				if ( ! $request->get_param( 'offset' ) ) {
-					$url = $request->get_param( 'q' );
-					$json = $this->get_json( $url, crc32( $url ) );
-					if ( ! is_wp_error( $json ) && isset( $json['id'], $json['attributedTo'] ) ) {
-						$user_id = $this->get_acct( $json['attributedTo'] );
-						$ret['statuses'][] = $this->convert_activity_to_status(
-							array(
-								'id'     => $json['id'] . '#create-activity',
-								'object' => $json,
 
-							),
-							$user_id
-						);
-					}
+		$q = $request->get_param( 'q' );
+		$query_is_url = parse_url( $q );
+		if ( $query_is_url ) {
+			if ( 'true' !== $request->get_param( 'resolve' ) || ! is_user_logged_in() ) {
+				return $ret;
+			}
+			$status = $this->get_json( $q, 'status-' . md5( $q ) );
+			$ret['statuses'][] = $this->convert_activity_to_status( array( 'object' => $status ), $status['attributedTo'] );
+		} else {
+			if ( ! $type || 'accounts' === $type ) {
+				if ( preg_match( '/^@?' . self::ACTIVITYPUB_USERNAME_REGEXP . '$/i', $q ) && ! $request->get_param( 'offset' ) ) {
+					$ret['accounts'][] = $this->get_friend_account_data( $q, array(), true );
 				}
-			} elseif ( is_user_logged_in() ) {
-				$args['s'] = $request->get_param( 'q' );
-				$args['offset'] = $request->get_param( 'offset' );
-				$args['posts_per_page'] = $request->get_param( 'limit' );
-				$ret['statuses'] = array_merge( $ret['statuses'], $this->get_posts( $args ) );
+				$query = new \WP_User_Query(
+					array(
+						'search'         => '*' . $q . '*',
+						'search_columns' => array(
+							'user_login',
+							'user_nicename',
+							'user_email',
+							'user_url',
+							'display_name',
+						),
+						'offset'         => $request->get_param( 'offset' ),
+						'number'         => $request->get_param( 'limit' ),
+					)
+				);
+				$users = $query->get_results();
+				foreach ( $users as $user ) {
+					$ret['accounts'][] = $this->get_friend_account_data( $user->ID );
+				}
+			}
+			if ( ! $type || 'statuses' === $type ) {
+				$args = $this->get_posts_query_args( $request );
+				if ( empty( $args ) ) {
+					return array();
+				}
+				$args = apply_filters( 'mastodon_api_timelines_args', $args, $request );
+				$valid_url = wp_parse_url( $q );
+				if ( $valid_url && isset( $valid_url['host'] ) ) {
+					if ( ! $request->get_param( 'offset' ) ) {
+						$url = $q;
+						$json = $this->get_json( $url, crc32( $url ) );
+						if ( ! is_wp_error( $json ) && isset( $json['id'], $json['attributedTo'] ) ) {
+							$user_id = $this->get_acct( $json['attributedTo'] );
+							$ret['statuses'][] = $this->convert_activity_to_status(
+								array(
+									'id'     => $json['id'] . '#create-activity',
+									'object' => $json,
+
+								),
+								$user_id
+							);
+						}
+					}
+				} elseif ( is_user_logged_in() ) {
+					$args['s'] = $q;
+					$args['offset'] = $request->get_param( 'offset' );
+					$args['posts_per_page'] = $request->get_param( 'limit' );
+					$ret['statuses'] = array_merge( $ret['statuses'], $this->get_posts( $args ) );
+				}
 			}
 		}
 		return $ret;
