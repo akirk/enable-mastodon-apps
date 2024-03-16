fc1455a36ea6bff2110b86b4d2015dcb1490e23e
Ralf Wiechers <ralf@cross-media-cloud.de>
2024-03-16 15:25:46 +0100

    remove method copied from template not needed

--- includes/handler/class-notification.php
+++ includes/handler/class-notification.php
@@ -24,44 +24,12 @@ class Notification {
 	}
 
 	public function register_hooks() {
-		add_filter( 'mastodon_api_notification', array( $this, 'api_notification' ), 10, 2 );
 
 		add_action( 'mastodon_api_notification_clear', array( $this, 'notification_clear' ), 20, 1 );
 		add_action( 'mastodon_api_notification_dismiss', array( $this, 'notification_dismiss' ), 20, 1 );
 
 		add_filter( 'mastodon_api_notification_get', array( $this, 'notification_get' ), 20, 2 );
 		add_filter( 'mastodon_api_notifications_get', array( $this, 'notifications_get' ), 20, 2 );
-	}
-
-	public function api_notification( $user_data, $user_id ) {
-		$user = get_user_by( 'ID', $user_id );
-
-		if ( ! $user ) {
-			return $user_data;
-		}
-
-		$notification = new Notification_Entity();
-		$notification->id             = strval( $user->ID );
-		$notification->username       = $user->user_login;
-		$notification->display_name   = $user->display_name;
-		$notification->avatar         = get_avatar_url( $user->ID );
-		$notification->avatar_static  = get_avatar_url( $user->ID );
-		$notification->acct           = $user->user_login;
-		$notification->note           = get_user_meta( $user->ID, 'description', true );
-		$notification->created_at     = new \DateTime( $user->user_registered );
-		$notification->statuses_count = count_user_posts( $user->ID, 'post', true );
-		$notification->last_status_at = new \DateTime( $user->user_registered );
-		$notification->url            = get_author_posts_url( $user->ID );
-
-		$notification->source = array(
-			'privacy'   => 'public',
-			'sensitive' => false,
-			'language'  => get_user_locale( $user->ID ),
-			'note'      => '',
-			'fields'    => array(),
-		);
-
-		return $notification;
 	}
 
 	/**
