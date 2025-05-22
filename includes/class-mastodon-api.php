15aee593e0f16c384be6dc593ad059f1bf95b7dd
Alex Kirk <akirk@users.noreply.github.com>
2025-05-22 16:14:30 +0200

    Add the editor to Mastodon posts

--- includes/class-mastodon-api.php
+++ includes/class-mastodon-api.php
@@ -245,7 +245,7 @@ class Mastodon_API {
 			'show_in_rest' => false,
 			'rewrite'      => false,
 			'menu_icon'    => 'dashicons-megaphone',
-			'supports'     => array( 'post-formats', 'comments', 'revisions', 'author' ),
+			'supports'     => array( 'editor', 'post-formats', 'comments', 'revisions', 'author' ),
 		);
 		register_post_type( self::POST_CPT, $args );
 
