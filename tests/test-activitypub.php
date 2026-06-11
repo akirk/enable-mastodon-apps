7e2073ac224d4c6e689d80ee139a7fd73503df08
Alex Kirk <akirk@users.noreply.github.com>
2026-06-11 14:02:33 +0200

    Pass outbox item to ActivityPub processor

--- tests/test-activitypub.php
+++ tests/test-activitypub.php
@@ -159,7 +159,7 @@ class ActivityPub_Test extends Mastodon_API_TestCase {
 		$this->assertNotEmpty( $outbox );
 		$json = json_decode( $outbox[0]->post_content );
 		$this->assertEquals( $json->object->url, home_url( '?c=' . $comment->comment_ID ) );
-		do_action( 'activitypub_process_outbox' );
+		do_action( 'activitypub_process_outbox', $outbox[0]->ID );
 
 		$this->assertEquals( 'federate', substr( get_comment_meta( $comment->comment_ID, 'activitypub_status', true ), 0, 8 ) );
 
