bc910ac408a2ca9d7c6f4b065a507e8935e65156
Alex Kirk <akirk@users.noreply.github.com>
2026-07-21 22:23:49 +0200

    Fix profile image updates from Mastodon clients

--- includes/handler/class-account.php
+++ includes/handler/class-account.php
@@ -78,6 +78,19 @@ class Account extends Handler {
 		$account->display_name   = $user->display_name;
 		$account->avatar         = get_avatar_url( $user->ID );
 		$account->avatar_static  = get_avatar_url( $user->ID );
+		$avatar = get_user_meta( $user->ID, 'mastodon_api_avatar', true );
+		if ( is_array( $avatar ) && ! empty( $avatar['full'] ) ) {
+			$account->avatar        = $avatar['full'];
+			$account->avatar_static = $avatar['full'];
+		}
+		$header_id = absint( get_user_meta( $user->ID, 'mastodon_api_header_id', true ) );
+		if ( $header_id ) {
+			$header_url = wp_get_attachment_url( $header_id );
+			if ( $header_url ) {
+				$account->header        = $header_url;
+				$account->header_static = $header_url;
+			}
+		}
 		$account->acct           = $user->user_login;
 		$account->note           = wpautop( $note );
 		$account->created_at     = new \DateTime( $user->user_registered );
